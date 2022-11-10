42abe82c6e807658246ec3fba366132a305ce396
karthik g.k <kar221694@gmail.com>
2022-11-10 12:23:41 -0500

    modified

---
+++ color-picker/files/html.js
@@ -0,0 +1,8 @@
+/*const input = document.querySelector('.color');
+console.log(input);
+
+input.addEventListener('onChange', ()=>{
+    console.log(input.value);
+});*/
+
+console.log("hello world");
--- color-picker/index.js
+++ color-picker/index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
 const data = require('./data.json');
+const bodyparser = require('body-parser');
+const path = require('path')
+
+app.use(bodyparser.urlencoded({extended:false}));
 
 app.get('/:color' , (req, res, next) => {
     const {color} = req.params;
     const obj = data.find((ele)=> ele.color === color);
-        
-})
+    res.send(obj);
+});
 
-app.listen(3000);
+app.use(express.static('files'));
+
+app.get('/',(req, res, next) => {
+    res.sendFile(path.join(__dirname , '/files' , '/index.html'));
+});
+
+app.use((req, res, next) => {
+    res.send(req.url)
+})
+app.listen(3000);   
