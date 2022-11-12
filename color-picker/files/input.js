7213e78ebd02c6abf6b768ea99f2fbcd17d8f820
karthik g.k <kar221694@gmail.com>
2022-11-12 07:19:47 -0500

    added new feauters

--- color-picker/files/input.js
+++ color-picker/files/input.js
@@ -1,8 +1,22 @@
 const input = document.querySelector('.color');
 const button = document.querySelector('.button');
+const button2 = document.querySelector('.button2')
+const button3 = document.querySelector('.button3')
 
 button.addEventListener('click', (event)=>{
-     document.getElementById('body').style.backgroundColor = input.value;
+     document.getElementById('form').method = `GET`;
      document.getElementById('form').action = `/${input.value}`;
 });
+
+button2.addEventListener('click', (event)=>{
+     document.getElementById('form').method = `POST`;
+     document.getElementById('form').action = `/${input.value}`;
+});
+button3.addEventListener('click', (event)=>{
+     document.getElementById('form').method = `DELETE`;
+     document.getElementById('form').action = `/${input.value}`;
+});
+
+
+
 
--- color-picker/index.js
+++ color-picker/index.js
@@ -5,19 +5,54 @@ const bodyparser = require('body-parser');
 const path = require('path')
 
 app.use(bodyparser.urlencoded({extended:false}));
-
 app.use(express.static(path.join(__dirname, 'files')));
 
+
+/*home page*/
+app.get('/' , (req, res) => {
+    res.sendFile(path.join(__dirname,'/index.html'));
+})
+
+
+/* get all color codes */
+app.get('/all-colors' , (req, res, next) => {
+     res.json(data)
+});
+
+
+/* get particular code */
 app.get('/:color' , (req, res, next) => {
-    console.log(req.query);
     const {color} = req.params;
     const obj = data.find((ele)=> ele.color === color);
     res.json(obj);
 });
 
-app.get('/',(req, res, next) => {
-    res.sendFile(path.join(__dirname , '/index.html'));
+/* adding a color and value */
+app.post('/:color', (req, res) => {
+    const text = req.body;
+    data.push(text);
+    res.redirect('/all-colors')
 });
+
+/* deleting a color */
+app.delete('/:color', (req, res) => {
+    const {color} = req.params;
+    const filterdata = data.filter(ele => ele.color.toString() !== color.toString());
+    data = filterdata.slice();
+    res.send(filterdata) 
+})
+
+
+/* updating a color*/
+app.put('/:color', (req, res) => {
+    const {color} = req.params;
+    const id = req.body;
+    const newdata = data.map((ele)=> ele.color === color ? ele.value = id:ele);
+    res.send(newdata)
+})
+
+
+
 
 app.use((req, res, next) => {
     res.send(req.url)
