950d5fa71656d74aa069db3d09ca45aab9a0dad0
karthik g.k <kar221694@gmail.com>
2022-11-22 07:31:34 -0500

    added new

--- userlogin/index.js
+++ userlogin/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 const path = require("path");
+const bodyparser = require('body-parser');
 
+app.use(bodyparser.json());
 app.use(express.static(path.join(__dirname,'public')));
+
+app.post('/api/register',(req, res) => {
+      res.json({status:'OK'});
+});
+
 
 
 app.listen(3000, ()=>{
