10a0f039a8b8fa296d08ca9600e8ed5c3b430681
karthik g.k <kar221694@gmail.com>
2022-12-05 10:23:07 -0500

    connected to database

--- userlogin/index.js
+++ userlogin/index.js
@@ -2,15 +2,26 @@ const express = require('express')
 const app = express()
 const path = require("path");
 const bodyparser = require('body-parser');
+const mongoose = require("mongoose");
 
+
+try{
+    main();
+}
+catch(err){
+    console.log(err.message);
+}
+
+
+async function main(){
+ await mongoose.connect("mongodb+srv://karthikgk:karthik@cluster0.nxuwhxd.mongodb.net/?retryWrites=true&w=majority")
+}
 app.use(bodyparser.json());
-app.use(express.static(path.join(__dirname,'public')));
+app.use('/', express.static(path.join(__dirname,'public')));
 
 app.post('/api/register',(req, res) => {
       res.json({status:'OK'});
 });
-
-
 
 app.listen(3000, ()=>{
     console.log("server is running....");
