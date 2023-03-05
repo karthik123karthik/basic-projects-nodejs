dbd4656473c951139300286c20d7f5a0599debab
karthik g k <kar221694@gmail.com>
2023-03-05 22:53:05 +0530

    not to use passportjs

--- authentication/app/index.js
+++ authentication/app/index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express()
 const {connectToDatabase, User} = require('./database')
+const passport = require("passport");
+const {initializePassport} = require("../authenticate/init")
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.set("view engine", "ejs")
 
 connectToDatabase()
+
+initializePassport(passport);
 
 
 app.get('/',(req,res)=>{
@@ -15,6 +20,23 @@ app.get('/',(req,res)=>{
 
 app.get('/register',(req, res)=>{
   res.render("register")
+})
+
+app.post("/login",async (req, res)=>{
+   const user = await User.findOne(req.body);
+   if(user){
+    res.redirect("/");
+    return;
+   }
+   else{
+    res.status(404).send("user not found");
+    return;
+   }
+})
+
+
+app.get("/login",(req, res)=>{
+  res.render("login")
 })
 
 app.post('/register',async (req, res)=>{
--- authentication/authenticate/init.js
+++ authentication/authenticate/init.js
@@ -1,34 +1,30 @@
-const passport = require('passport')
-const bcrypt = require('bcrypt');
-const LocalStrategy = require('passport-local').Strategy;
+const Localstrategy = require('passport-local').Strategy
+const {User} = require('../app/database');
 
-const User  = {
-    username:"test name",
-    passwordHash:"bcrypted hash password",
-    id:1
-}
+exports.initializePassport = (passport)=>{
+   passport.use(new Localstrategy(async(username, password, done)=>{
+    try{
+    const user = await User.findOne({username:username});
+    if(!user)return done(null, false);    
+    if(user.password !== password)return done(null, false)
+    return done(null, user);
+    }
+    catch(err){
+        return done(err, false);
+    }
+   }));
 
-passport.use(new LocalStrategy(
-(username, password, done)=>{
-    findUser(username, (err, user)=>{
-        if(err){
-            return done(err)
-        }
+   passport.serializeUser((user,done)=>{
+    done(null, user.id);
+   })
 
-        if(!user){
-            return done(null, false)
-        }
-
-        bcrypt.compare(password, User.passwordHash, (err, isValid)=>{
-            if (err) {
-                return done(err)
-              }
-              if (!isValid) {
-                return done(null, false)
-              }
-              return done(null, user)
-        })
-    })
-}
-))
-
+   passport.deserializeUser(async(id, done)=>{
+    try{
+        const user = await User.findById(id);
+        done(null, user);
+    }
+    catch(err){
+        done(err, false);
+    }
+   })
+};
