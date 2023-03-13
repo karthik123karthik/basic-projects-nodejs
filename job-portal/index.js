5a0e49fb2d338c96fda9a5cf9709d796264c6661
karthik g k <kar221694@gmail.com>
2023-03-13 16:38:35 +0530

    made some css changes

--- job-portal/index.js
+++ job-portal/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 const ejs = require("ejs")
+const http = require('http')
+const server = http.createServer(app);
+const {Server} = require('socket.io');
+const io = new Server(server);
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"))
@@ -9,7 +13,18 @@ app.get('/',(req, res)=>{
     res.render("index")
 })
 
+io.on('connection',(socket)=>{
+    console.log("new User connected")
+    socket.on('disconnect',()=>{
+        console.log("User disconnected")
+    })
 
-app.listen(3000,()=>{
+    socket.on('chat message', (msg)=>{
+        io.emit('chat message',msg)
+    })
+})
+
+
+server.listen(3000,()=>{
     console.log("listening on port 3000")
 })
