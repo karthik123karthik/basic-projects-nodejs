e8505228a5f0fcecb36f569f9cd905efdf7c4a1c
karthik g k <kar221694@gmail.com>
2023-03-18 15:09:32 +0530

    trying to give a username

--- chat-app/index.js
+++ chat-app/index.js
@@ -7,7 +7,7 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const mongoose = require("mongoose");
 const {User,Conversation} = require('./Model/index')
-
+const bodyparser =  require('body-parser');
 
 
 // database related ---------
@@ -15,6 +15,7 @@ const connectttodatabase = async () => {
   try {
     const db = await mongoose.connect("mongodb://0.0.0.0:27017/chat-app");
     console.log("connected");
+    await  Conversation.deleteMany({});
   } catch (err) {
     console.log(err);
   }
@@ -28,10 +29,19 @@ connectttodatabase();
 // serving static filezs
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(bodyparser.urlencoded({extended:false}));
 
+app.get("/",(req, res)=>{
+  res.render("login");
+})
+let user;
 
+app.post("/chat",(req,res)=>{
+   user = req.body.username;
+   res.redirect("/chat");
+});
 
-app.get("/", async (_, res) => {
+app.get("/chat", async (_, res) => {
   const arr = await Conversation.find({});
   res.render("index",{conversations: arr});
 });
@@ -39,13 +49,13 @@ app.get("/", async (_, res) => {
 /////// socket.io connection
 io.on("connection", async(socket) => {
   try{
-  let newmessage = new Conversation({type:"notification", user:`${socket.id}`, message:`${socket.id} joined`});
+  let newmessage = new Conversation({type:"notification", user:`${user}`, message:`${user} joined`});
    await newmessage.save();
-  io.emit("new user", socket.id);
+  io.emit("new user", user);
   socket.on("disconnect", async() => {
-    let newmessage = new Conversation({type:"notification", user:`${socket.id}`, message:`${socket.id} left`});
+    let newmessage = new Conversation({type:"notification", user:`${user}`, message:`${user} left`});
     await newmessage.save();
-    io.emit("disconnected", socket.id);
+    io.emit("disconnected", user);
   });
 
   socket.on("chat message", async(msg) => {
@@ -58,6 +68,8 @@ catch(err){
   console.log(err);
 }
 });
+
+
 ///////////////////////////////////////////////////////
 
 server.listen(3000, () => {
